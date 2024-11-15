f4618e92a27d200ceb378f3e8ccda2785c6dbef8
Yvan Wang <skiingwang@163.com>
2024-11-15 13:44:31 +0000

    done

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,15 @@ import numpy as np
 
 def calculate(list):
 
+    target = np.array(list).reshape(3,3)
 
+    result = {
+        'mean': [np.mean(target, axis=0).tolist(), np.mean(target, axis=1).tolist(), np.mean(target)],
+        'variance': [np.var(target, axis=0).tolist(), np.var(target, axis=1).tolist(), np.var(target)],
+        'standard deviation': [np.std(target, axis=0).tolist(), np.std(target, axis=1).tolist(), np.std(target)],
+        'max': [np.max(target, axis=0).tolist(), np.max(target, axis=1).tolist(), np.max(target)],
+        'min': [np.min(target, axis=0).tolist(), np.min(target, axis=1).tolist(), np.min(target)],
+        'sum': [np.sum(target, axis=0).tolist(), np.sum(target, axis=1).tolist(), np.sum(target)]
+    }
 
-
-    return calculations
+    return result
